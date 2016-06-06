ce6b81facf4620c1c20057ad2e9aeae85ea9d33b
matike <matike@users.noreply.github.com>
2016-06-06 10:18:06 +0300

    testing
    testing

--- java_gui/src/ilmoitin/Main.java
+++ java_gui/src/ilmoitin/Main.java
@@ -5,5 +5,6 @@ import javax.swing.SwingUtilities;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new SimpleGuiApp());
+        System.out.println("muutos");
     }
 }
