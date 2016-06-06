6fa84683e773ecbe698d9e65b1a96b4470c4f019
matike <matike@users.noreply.github.com>
2016-06-06 10:37:16 +0300

    updatetesting
    testing

--- trivial/src/Trivial.java
+++ trivial/src/Trivial.java
@@ -3,9 +3,6 @@ public class Trivial {
     // Make this return 1 to make the test pass.
     public int f() {
         //BEGIN SOLUTION
-        return 1;
-        if (1+1==1) {
-        return 0;
-        }
+        return 3;
     }
 }
