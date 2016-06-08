cb22fcfcb18997247d0a4a6e591dc5d58c32e0d0
matike <matike@users.noreply.github.com>
2016-06-08 10:26:18 +0300

    Update Arith.java

--- arith_funcs/src/Arith.java
+++ arith_funcs/src/Arith.java
@@ -14,16 +14,16 @@ public class Arith {
         // STUB: return 0;
     }
     
-    public static int mul(int a, int b) {
+    public static int mul(int aaa, int b) {
         // BEGIN SOLUTION
-        return a * b;
+        return aaa * b;
         // END SOLUTION
         // STUB: return 0;
     }
     
-    public static int div(int a, int b) {
+    public static int div(int a, int bbb) {
         // BEGIN SOLUTION
-        return a / b;
+        return a / bbb;
         // END SOLUTION
         // STUB: return 0;
     }
